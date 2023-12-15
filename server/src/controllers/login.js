a2217198c340a187e813a25351c32ad0b8e71a51
Eduardo Hernandez <zohanshane@gmail.com>
2023-12-15 15:23:23 -0600

    server and client

---
+++ server/src/controllers/login.js
@@ -0,0 +1,16 @@
+const users = require("../utils/users.js");
+
+
+module.exports = (req, res) => {
+    // req.query = { email: "mail@.com" , password:"1234"}
+    const {email , password} = req.query;
+    let access = false;
+
+    users.forEach(user => {
+        if(user.email === email && user.password == password) 
+        access = true;
+    });
+
+    return res.json({ access });
+
+};
